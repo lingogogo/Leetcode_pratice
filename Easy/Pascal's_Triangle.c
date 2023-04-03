4d610cdde81eccf671434d73e25a2cd153f90a69
lingogogo <89990453+lingogogo@users.noreply.github.com>
2023-04-03 10:54:03 +0800

    Create Pascal's_Triangle.c

---
+++ Easy/Pascal's_Triangle.c
@@ -0,0 +1,31 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** generate(int numRows, int* returnSize, int** returnColumnSizes){
+    *returnSize = numRows;
+
+    *returnColumnSizes = (int*)malloc(numRows * sizeof(int));
+    for (int i = 0; i < numRows; i ++) {
+        (*returnColumnSizes)[i] = i + 1;
+    }
+
+    int **ans = (int**) malloc(numRows* sizeof(int*));
+    
+    for(int i =0; i < numRows;i++)
+    {
+        ans[i] = (int*)malloc((i+1)*sizeof(int));
+        ans[i][i] = 1;
+        ans[i][0] = 1;
+    }
+
+    for(int i = 2;i < numRows;i++)
+    {
+        for(int j = 1;j<i;j++)
+        {
+            ans[i][j] = ans[i-1][j-1]+ ans[i-1][j];
+        }
+    }
+    return ans;
+}
