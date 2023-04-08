a78855b9c30ca2e95068c706bd986d99310552fa
lingogogo <89990453+lingogogo@users.noreply.github.com>
2023-04-08 11:14:55 +0800

    Create hasCycle.cpp

---
+++ Easy/hasCycle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+	bool hasCycle(ListNode *head) {
+        while (head) {
+            if (head == head->next)
+                return true;
+            tmp = head->next;
+            head->next = head;
+            head = tmp;
+        }
+        return false;
+    }
+private:
+    ListNode *tmp = nullptr;
+
+};
