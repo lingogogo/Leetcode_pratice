c4604c1e198ab58976c204f11aa03fa05e0a1907
SyueCian <89990453+lingogogo@users.noreply.github.com>
2023-06-03 11:20:09 +0800

    Create sumOfMultiples.c

---
+++ Easy/sumOfMultiples.c
@@ -0,0 +1,13 @@
+int sumOfMultiples(int n){
+    int sum = 0;
+    int con = 0;
+    for(int i = 1; i <= n; i ++)
+    {
+        if((i%3 == 0|| i%5 == 0) || i%7 == 0)
+        {
+            sum+=i;
+        }
+
+    }
+    return sum;
+}
