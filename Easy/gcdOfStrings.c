d51d20b268f6de3b4da41608105299c7d1d284fe
SyueCian <89990453+lingogogo@users.noreply.github.com>
2023-07-19 21:24:34 +0800

    Update gcdOfStrings.c
    New version for the new data input

--- Easy/gcdOfStrings.c
+++ Easy/gcdOfStrings.c
@@ -5,6 +5,13 @@ char * gcdOfStrings(char * str1, char * str2){
     if(len1 < len2) return gcdOfStrings(str2,str1);
     if(!strcmp(str1,str2)) return str2;
     else if(str1[0] != str2[0]) return "";
+    else
+    {
+        for(int i = 0; i < len2 ; i++)
+        {
+            if(str1[i] != str2[i]) return "";
+        }
+    }
     return gcdOfStrings(str1 + len2, str2);
 
     
