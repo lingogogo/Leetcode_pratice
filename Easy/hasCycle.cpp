c2fc2c4547ab3a647c173b2f01a87215d2fb2e85
SyueCian <89990453+lingogogo@users.noreply.github.com>
2023-11-02 15:58:04 +0800

    Update hasCycle.cpp

--- Easy/hasCycle.cpp
+++ Easy/hasCycle.cpp
@@ -13,3 +13,39 @@ public:
 private:
     ListNode *tmp = nullptr;
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* first_p = head;
+        ListNode* sec_p = head;
+        if(head == nullptr)
+        {
+            return false;
+        }
+        while(true)
+        {
+            if(first_p->next != nullptr && sec_p->next != nullptr && sec_p->next->next!=nullptr)
+            {
+                first_p = first_p->next;
+                sec_p = sec_p->next->next;
+            }else if(first_p->next == nullptr || sec_p->next == nullptr || sec_p->next->next == nullptr )
+            {
+                return false;
+            }
+
+            if(first_p == sec_p)
+            {
+                return true;
+            }
+        }
+    }
+};
