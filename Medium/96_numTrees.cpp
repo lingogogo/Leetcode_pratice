24f50a7ae0990d0f55aaa31c62c0b5c59036ae23
SyueCian <89990453+lingogogo@users.noreply.github.com>
2025-04-26 13:03:04 +0800

    Create 96_numTrees.cpp

---
+++ Medium/96_numTrees.cpp
@@ -0,0 +1,48 @@
+// class Solution {
+// public:
+//     int numTrees(int n) {
+//         // Check the right and left tree.
+//         int output =0;
+//         vector<int> dp(2,0);
+//         // dp[0] left;
+//         // dp[1] right;
+
+
+        
+
+//         return output;
+//     }
+// };
+// https://leetcode.com/problems/unique-binary-search-trees/solutions/6654202/recursion-memorization-c-intuitive-solution
+class Solution {
+public:
+    int numTrees(int n) {
+        int l=1;
+        int r=n;
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        int output = totalBST(n,l,r,dp);
+        // cout << dp[l][r] << endl;
+        // cout << dp[r - 1][r] << endl;
+        for(int i = 0; i <= n; i++){
+            for(int j = 0;j <= n; j++)
+            {
+                cout << dp[i][j] << " ";
+            }
+            cout << endl;
+        }
+        return output;
+    }
+
+    int totalBST(int n, int l, int r, vector<vector<int>> &dp){
+        // cout << l<< " " << r << endl;
+        if(l>r || l==r) return 1; // The void tree is also a tree
+        if(dp[l][r]!=-1) return dp[l][r];
+        int ans=0;
+        for(int i=l;i<=r;i++){
+            int left = totalBST(n,l,i-1,dp);
+            int right = totalBST(n,i+1,r,dp);
+            ans += left*right;
+        }
+        return dp[l][r] = ans;
+    }
+};
