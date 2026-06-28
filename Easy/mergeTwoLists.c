21f136fbcbb31f05c37bb9f47c67546561291484
SyueCian <89990453+lingogogo@users.noreply.github.com>
2026-06-28 23:53:36 +0800

    Update mergeTwoLists.c

--- Easy/mergeTwoLists.c
+++ Easy/mergeTwoLists.c
@@ -1,3 +1,4 @@
+// Method1: iterations
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -42,4 +43,36 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
     }
 
     return dummynode.next;
+}
+// Method2: Recursive
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+    if(list1 == NULL)
+    {
+        return list2;
+    }
+    if(list2 == NULL)
+    {
+        return list1;
+    }
+
+    if(list1->val <= list2->val)
+    {
+        list1->next = mergeTwoLists(list1->next,list2);
+        return list1;
+    }
+
+    if(list1->val > list2->val)
+    {
+        list2->next = mergeTwoLists(list1, list2->next);
+        return list2;
+    }
+
+    return list1;
 }
