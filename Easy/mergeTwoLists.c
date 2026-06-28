64722ccde5255261ac1d9a99a89ae139a60d1cfd
SyueCian <89990453+lingogogo@users.noreply.github.com>
2026-06-28 23:51:10 +0800

    Add mergeTwoLists function for linked list merging
    Implemented a function to merge two sorted linked lists into one sorted linked list.

---
+++ Easy/mergeTwoLists.c
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    // Trivial solution due to empty list
+    if(list1 == NULL) return list2;
+    else if(list2 == NULL) return list1;
+
+    // movenode is the iterations pointer.
+    struct ListNode dummynode;
+    struct ListNode* movenode = &dummynode;
+    while(list1 != NULL && list2 != NULL)
+    {
+        if(list1->val <= list2->val)
+        {
+            movenode->next = list1;
+            list1 = list1->next;
+            movenode = movenode->next;
+        }else{
+            movenode->next = list2;
+            list2 = list2->next;
+            movenode = movenode->next;
+        }
+    }
+    // Remaining list1
+    while(list1!= NULL)
+    {
+        movenode->next = list1;
+        list1 = list1->next;
+        movenode = movenode->next;
+    }
+    // Remaining list2
+    while(list2!= NULL)
+    {
+        movenode->next = list2;
+        list2 = list2->next;
+        movenode = movenode->next;
+    }
+
+    return dummynode.next;
+}
